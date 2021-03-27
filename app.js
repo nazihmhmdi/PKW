cb33b8e3a76c38e3f1487efbbc16d6ae58c2d203
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmali.com>
2021-03-27 21:16:55 +0700

    Bio

---
+++ app.js
@@ -0,0 +1,32 @@
+console.log('console log app.js')
+
+//menampilkan alert popup
+//  alert('Haloooo')
+
+//menampilkan confirm popup
+// confirm('Yakin doang eh enggak yakin banget?')
+
+let a = 1
+let b = 2
+let c = a + b
+console.log(c)
+
+Date()
+let tanggal = Date()
+alert(tanggal)
+
+document.getElementById('nama').innerHTML = "Nazih"
+
+document.getElementById('nama').style.color="lightcoral"
+
+document.getElementById('nama').style.backgroundImage="url(https://i.pinimg.com/originals/8d/a1/58/8da1582d719ae991452e8863cfd7e35a.jpg)"
+
+function hey(){
+    alert('Ini Funct')
+}
+
+let clickme = document.getElementById('click-me')
+
+clickme.addEventListener('click', function nama(){
+    console.log('coba')
+})
